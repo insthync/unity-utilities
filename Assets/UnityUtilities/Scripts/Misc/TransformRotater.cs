2bef04bbaefda65f57cdc207d3772c71b25599eb
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-06-06 05:18:16 +0700

    Demo contents & Transform Rotator

---
+++ Assets/UnityUtilities/Scripts/Misc/TransformRotater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformRotater : MonoBehaviour {
+    public Vector3 rotatingAxis;
+    private Transform cacheTransform;
+    private void Awake()
+    {
+        cacheTransform = transform;
+    }
+
+    private void Update()
+    {
+        cacheTransform.Rotate(rotatingAxis * Time.deltaTime);
+    }
+}
