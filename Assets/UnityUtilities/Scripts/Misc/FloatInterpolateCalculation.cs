dcbd1f616489a173c5085959bfc0c1b8037e7264
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-06-09 21:24:56 +0700

    Split Game formula class

--- Assets/UnityUtilities/Scripts/Misc/FloatInterpolateCalculation.cs
+++ Assets/UnityUtilities/Scripts/Misc/FloatInterpolateCalculation.cs
@@ -1,22 +1,22 @@
 ﻿using UnityEngine;
 
-public static class GameFormula
+[System.Serializable]
+public class FloatInterpolateCalculation
 {
-    public static int CalculateInterpolate(int min_attr, int max_attr, int current_level, int max_level, float growth_factor)
-    {
-        if (current_level < 1)
-            current_level = 1;
-        if (max_level < 2)
-            max_level = 2;
+    public int maxLevel;
+    public float minValue;
+    public float maxValue;
+    public float growthValue;
 
-        if (current_level > max_level)
+    public float[] GenerateValueArray()
+    {
+        float[] input = new float[maxLevel];
+        for (int i = 0; i < input.Length; ++i)
         {
-            return CalculateInterpolate(min_attr, max_attr, max_level, max_level, growth_factor);
+            float value = CalculateInterpolate(minValue, maxValue, i + 1, maxLevel, growthValue);
+            input[i] = value;
         }
-        else
-        {
-            return min_attr + Mathf.CeilToInt((max_attr - min_attr) * Mathf.Pow((float)(current_level - 1) / (float)(max_level - 1), growth_factor));
-        }
+        return input;
     }
 
     public static float CalculateInterpolate(float min_attr, float max_attr, int current_level, int max_level, float growth_factor)
---
+++ Assets/UnityUtilities/Scripts/Misc/IntegerInterpolateCalculation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class IntegerInterpolateCalculation
+{
+    public int maxLevel;
+    public int minValue;
+    public int maxValue;
+    public float growthValue;
+
+    public int[] GenerateValueArray()
+    {
+        int[] input = new int[maxLevel];
+        for (int i = 0; i < input.Length; ++i)
+        {
+            int value = CalculateInterpolate(minValue, maxValue, i + 1, maxLevel, growthValue);
+            input[i] = value;
+        }
+        return input;
+    }
+
+    public static int CalculateInterpolate(int min_attr, int max_attr, int current_level, int max_level, float growth_factor)
+    {
+        if (current_level < 1)
+            current_level = 1;
+        if (max_level < 2)
+            max_level = 2;
+
+        if (current_level > max_level)
+        {
+            return CalculateInterpolate(min_attr, max_attr, max_level, max_level, growth_factor);
+        }
+        else
+        {
+            return min_attr + Mathf.CeilToInt((max_attr - min_attr) * Mathf.Pow((float)(current_level - 1) / (float)(max_level - 1), growth_factor));
+        }
+    }
+}
