44ea48638c9be1beda270e12c7950e0c0c79dc5a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-09-22 03:28:35 +0700

    Add Scroll rect by children component

---
+++ Assets/UnityUtilities/Scripts/Misc/ScrollRectByChildren.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollRectByChildren : MonoBehaviour, 
+    IBeginDragHandler, 
+    IDragHandler, 
+    IEndDragHandler, 
+    IScrollHandler
+{
+    public ScrollRect scrollRect;
+    private void Awake()
+    {
+        if (scrollRect == null)
+            scrollRect = GetComponentInParent<ScrollRect>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (scrollRect != null)
+            scrollRect.OnBeginDrag(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (scrollRect != null)
+            scrollRect.OnDrag(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (scrollRect != null)
+            scrollRect.OnEndDrag(eventData);
+    }
+
+    public void OnScroll(PointerEventData data)
+    {
+        if (scrollRect != null)
+            scrollRect.OnScroll(data);
+    }
+}
